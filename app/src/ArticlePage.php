3e6ac1e133a39bcd0ae6ba9e4bda0f8beb25025f
RichardHpa <richard.hpa@hotmail.com>
2020-02-14 15:41:24 +1300

    photos and a brochure can now be added and viewed on front end

--- app/src/ArticlePage.php
+++ app/src/ArticlePage.php
@@ -7,6 +7,10 @@ use SilverStripe\Forms\DateField;
 use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\TextField;
 
+use SilverStripe\Assets\Image;
+use SilverStripe\Assets\File;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+
 class ArticlePage extends Page{
     private static $can_be_root = false;
 
@@ -17,6 +21,17 @@ class ArticlePage extends Page{
         'AuthorName' => 'Varchar'
     ];
 
+    private static $has_one = [
+        'Photo' => Image::class,
+        'Brochure' => File::class
+    ];
+
+    // If we dont say this class owns the images, we wont see them unless we are logged in as admins
+    private static $owns = [
+        'Photo',
+        'Brochure',
+    ];
+
     //Adds a front end form through the admin
     public function getCMSFields(){
         $fields = parent::getCMSFields();
@@ -24,8 +39,23 @@ class ArticlePage extends Page{
         $fields->addFieldToTab('Root.Main', TextareaField::create('Teaser')->setDescription('This is the summary that appears on the article list page'), 'Content');
         $fields->addFieldToTab('Root.Main', TextField::create('AuthorName', 'Author of article'), 'Content');
 
+        $fields->addFieldToTab('Root.Attachments', $photo = UploadField::create('Photo'));
+        $fields->addFieldToTab('Root.Attachments', $brochure = UploadField::create(
+            'Brochure',
+            'Travel brochure, optional (PDF only)'
+        ));
+
+        $photo->setFolderName('travel-photos');
+        $brochure
+            ->setFolderName('travel-brochures')
+            ->getValidator()->setAllowedExtensions(['pdf']);
+
         return $fields;
     }
+
+
+
+
 
 
 }
