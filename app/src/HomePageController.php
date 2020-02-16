f94c7de98d90bcb38bdcbfb90ce2c9fd00835b55
RichardHpa <richard.hpa@hotmail.com>
2020-02-16 20:49:50 +1300

    Introduced the ORM to home page with recent articles

--- app/src/HomePageController.php
+++ app/src/HomePageController.php
@@ -5,5 +5,11 @@ namespace SilverStripe\Lessons;
 use PageController;
 
 class HomePageController extends PageController{
-    
+
+    public function LatestArticles($count = 3){
+        return ArticlePage::get()
+            ->sort('Created', 'DESC')
+            ->limit($count);
+    }
+
 }
